1e6cdb916d658735fa5fda51b795705a3b7e1d52
yubozhao <yubz86@gmail.com>
2014-11-22 11:40:08 -0800

    bump version to 0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Instagram account login for meteor",
-  "version": "0.1.3",
+  "version": "0.2.0",
   "git": "https://github.com/yubozhao/meteor-accounts-instagram",
   "name": "bozhao:accounts-instagram"
 });
